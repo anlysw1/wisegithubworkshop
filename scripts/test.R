5fce9bf37ad32b78e0cf214d707482b2cab102f9
anlysw1 <82343280+anlysw1@users.noreply.github.com>
2021-04-12 15:16:28 -0600

    Update test.R
    deleted line

--- scripts/test.R
+++ scripts/test.R
@@ -1,7 +1,5 @@
 # "This is an R script that we will make changes to during the Github workshop
 # for WISE -- THIS IS SO MUCH FUN !!!" - Courtney Collins 
 
-# This is a new line. 
-
 
 dat <- read.csv("data/data.csv")
