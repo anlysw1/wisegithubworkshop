d9c098f131430cff16434dc67d5b9ed1b57f5199
anlysw1 <82343280+anlysw1@users.noreply.github.com>
2021-04-12 15:22:12 -0600

    Update test.R

--- scripts/test.R
+++ scripts/test.R
@@ -1,5 +1,5 @@
 # "This is an R script that we will make changes to during the Github workshop
 # for WISE -- THIS IS SO MUCH FUN !!!" - Courtney Collins 
 
-# local work
+# on github
 dat <- read.csv("data/data.csv")
