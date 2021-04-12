7c4928d056cb8511e58a77ef27b4484f4eea11c6
Angie Swanson <anlysw1@gmail.com>
2021-04-12 15:20:03 -0600

    test

--- scripts/test.R
+++ scripts/test.R
@@ -1,6 +1,5 @@
 # "This is an R script that we will make changes to during the Github workshop
 # for WISE -- THIS IS SO MUCH FUN !!!" - Courtney Collins 
 
-# This is for the conflict between scripts.
-
+# local work
 dat <- read.csv("data/data.csv")
