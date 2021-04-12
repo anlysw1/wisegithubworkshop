0982864160b8870b318b74417ab4ad76cc5c8407
anlysw1 <82343280+anlysw1@users.noreply.github.com>
2021-04-12 15:18:05 -0600

    Update test.R

--- scripts/test.R
+++ scripts/test.R
@@ -1,5 +1,6 @@
 # "This is an R script that we will make changes to during the Github workshop
 # for WISE -- THIS IS SO MUCH FUN !!!" - Courtney Collins 
 
+# This is for the conflict between scripts.
 
 dat <- read.csv("data/data.csv")
